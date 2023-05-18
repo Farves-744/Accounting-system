b265a03980c28c87258c1390fe4be5d391c15b07
Farves-744 <128053117+Farves-744@users.noreply.github.com>
2023-05-18 09:10:04 +0530

    Update new-user.component.ts

--- src/app/modules/admin/roles-and-permissions/components/new-user/new-user.component.ts
+++ src/app/modules/admin/roles-and-permissions/components/new-user/new-user.component.ts
@@ -1,10 +1,13 @@
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { GetAccounts } from 'app/shared/modals/accounts';
 import { GetRole } from 'app/shared/modals/role';
+import { AccountService } from 'app/shared/services/account.service';
 import { CommonService } from 'app/shared/services/common.service';
 import { RoleService } from 'app/shared/services/role.service';
 import { UserService } from 'app/shared/services/user.service';
+import { MessageService } from 'primeng/api';
 
 @Component({
     selector: 'app-new-user',
@@ -17,13 +20,17 @@ export class NewUserComponent {
     userId: any;
     isEdit: boolean = false; // this is for toggle button text add or edit
     roleModel: GetRole = new GetRole();
+    getAccountsModel: GetAccounts = new GetAccounts();
     rolesData: any;
+    accounts: any;
 
     constructor(
         private _commonService: CommonService,
         private _userService: UserService,
         private _roleService: RoleService,
+        private _accountService: AccountService,
         private _formBuilder: FormBuilder,
+        private messageService: MessageService,
         private _route: Router,
         private changeDetection: ChangeDetectorRef
     ) { }
@@ -38,6 +45,7 @@ export class NewUserComponent {
             editPassword: ['', [Validators.required, Validators.minLength(6)]],
             roleId: [null, Validators.required],
             userId: this.userId,
+            accountId: [null, Validators.required]
         });
         this.getRolesName();
     }
@@ -82,11 +90,14 @@ export class NewUserComponent {
                     .updateUser(this.addUserForm.value)
                     .subscribe((res) => {
                         console.log(this._commonService.decryptData(res));
+                        this.messageService.add({ severity: 'success', summary: 'Success', detail: 'User updated successfully' });
                         this.addUserForm.reset();
                         this._route.navigateByUrl(
                             'roles-and-permissions/manage-user'
                         );
                         this.changeDetection.detectChanges();
+                    }, error => {
+                        this.messageService.add({ severity: 'error', summary: 'Failed', detail: 'Something went wrong' });
                     });
             } else {
                 console.log(this.addUserForm.value);
@@ -95,11 +106,14 @@ export class NewUserComponent {
                     .addUser(this.addUserForm.value)
                     .subscribe((res) => {
                         console.log(this._commonService.decryptData(res));
+                        this.messageService.add({ severity: 'success', summary: 'Success', detail: 'User added successfully' });
                         this.addUserForm.reset();
                         this._route.navigateByUrl(
                             'roles-and-permissions/manage-user'
                         );
                         this.changeDetection.detectChanges();
+                    }, error => {
+                        this.messageService.add({ severity: 'error', summary: 'Failed', detail: 'Something went wrong' });
                     });
             }
         }
@@ -108,6 +122,8 @@ export class NewUserComponent {
     navigateToHome() {
         this._commonService.navigateToHome();
     }
+
+
 
     getRolesName() {
         this.roleModel.userId = this.userId;
@@ -118,5 +134,22 @@ export class NewUserComponent {
             console.log(this.rolesData);
             this.changeDetection.detectChanges();
         });
+    }
+
+    getAccountName() {
+        console.log(this.getAccountsModel);
+
+        this.getAccountsModel.userId = this.userId;
+
+        this._accountService
+            .getAccount(this.getAccountsModel)
+            .subscribe((res) => {
+                console.log(this._commonService.decryptData(res));
+                this.accounts = this._commonService.decryptData(res);
+                console.log(this.accounts);
+                this.changeDetection.detectChanges();
+            }, (error) => {
+                console.log(error);
+            });
     }
 }
