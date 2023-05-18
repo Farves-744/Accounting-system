b23280dd68f8c9e99f9311a469b1727bb3f8d222
Farves-744 <128053117+Farves-744@users.noreply.github.com>
2023-05-18 09:00:21 +0530

    Update delete-dialog.component.ts

--- src/app/modules/admin/expense/components/delete-dialog/delete-dialog.component.ts
+++ src/app/modules/admin/expense/components/delete-dialog/delete-dialog.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { CommonService } from 'app/shared/services/common.service';
 import { ExpenseService } from 'app/shared/services/expense.service';
+import { MessageService } from 'primeng/api';
 
 @Component({
     selector: 'app-delete-dialog',
@@ -11,10 +12,11 @@ import { ExpenseService } from 'app/shared/services/expense.service';
 export class DeleteDialogComponent implements OnInit {
     constructor(
         private _expenseService: ExpenseService,
+        private messageService: MessageService,
         private _commonService: CommonService,
         public _dialogRef: MatDialogRef<DeleteDialogComponent>,
         @Inject(MAT_DIALOG_DATA) private data: any
-    ) {}
+    ) { }
 
     userId: any;
 
@@ -31,9 +33,10 @@ export class DeleteDialogComponent implements OnInit {
         this._expenseService.deleteCategory(req).subscribe(
             (res) => {
                 console.log(this._commonService.decryptData(res));
+                this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Income deleted successfully' });
             },
             (err) => {
-                console.error;
+                this.messageService.add({ severity: 'error', summary: 'You cannot delete', detail: 'This category is being used' });
             }
         );
         this._dialogRef.close(true);
