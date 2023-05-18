21d32ac070da25788f14806d44192306204c69e2
Farves-744 <128053117+Farves-744@users.noreply.github.com>
2023-05-18 08:56:29 +0530

    Update delete-dialog.component.ts

--- src/app/modules/admin/accounts/components/delete-dialog/delete-dialog.component.ts
+++ src/app/modules/admin/accounts/components/delete-dialog/delete-dialog.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { AccountService } from 'app/shared/services/account.service';
 import { CommonService } from 'app/shared/services/common.service';
+import { MessageService } from 'primeng/api';
 
 @Component({
     selector: 'app-delete-dialog',
@@ -11,10 +12,11 @@ import { CommonService } from 'app/shared/services/common.service';
 export class DeleteDialogComponent implements OnInit {
     constructor(
         private _accountService: AccountService,
+        private messageService: MessageService,
         private _commonService: CommonService,
         public _dialogRef: MatDialogRef<DeleteDialogComponent>,
         @Inject(MAT_DIALOG_DATA) private data: any
-    ) {}
+    ) { }
 
     userId: any;
 
@@ -31,9 +33,10 @@ export class DeleteDialogComponent implements OnInit {
         this._accountService.deleteAccount(req).subscribe(
             (res) => {
                 console.log(this._commonService.decryptData(res));
+                this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Account deleted successfully' });
             },
             (err) => {
-                console.error;
+                this.messageService.add({ severity: 'error', summary: 'You cannot delete', detail: 'This Account is being used' });
             }
         );
         this._dialogRef.close(true);
