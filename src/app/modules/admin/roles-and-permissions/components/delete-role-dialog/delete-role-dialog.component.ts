6714be271a63ae2cc3aaa2a9d2e9cb951a7151b4
Farves-744 <128053117+Farves-744@users.noreply.github.com>
2023-05-18 09:09:11 +0530

    Update delete-role-dialog.component.ts

--- src/app/modules/admin/roles-and-permissions/components/delete-role-dialog/delete-role-dialog.component.ts
+++ src/app/modules/admin/roles-and-permissions/components/delete-role-dialog/delete-role-dialog.component.ts
@@ -3,6 +3,7 @@ import { CommonService } from 'app/shared/services/common.service';
 import { RoleService } from 'app/shared/services/role.service';
 import { DeleteDialogComponent } from '../delete-dialog/delete-dialog.component';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MessageService } from 'primeng/api';
 
 @Component({
     selector: 'app-delete-role-dialog',
@@ -12,10 +13,11 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 export class DeleteRoleDialogComponent implements OnInit {
     constructor(
         private _roleService: RoleService,
+        private messageService: MessageService,
         private _commonService: CommonService,
         public _dialogRef: MatDialogRef<DeleteDialogComponent>,
         @Inject(MAT_DIALOG_DATA) private data: any
-    ) {}
+    ) { }
 
     userId: any;
 
@@ -32,9 +34,10 @@ export class DeleteRoleDialogComponent implements OnInit {
         this._roleService.deleteRole(req).subscribe(
             (res) => {
                 console.log(this._commonService.decryptData(res));
+                this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Role deleted successfully' });
             },
             (err) => {
-                console.error;
+                this.messageService.add({ severity: 'error', summary: 'You cannot delete', detail: 'This Role is being used' });
             }
         );
         this._dialogRef.close(true);
